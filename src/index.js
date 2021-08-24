a6bd76d801048e1c327e46aa9a47d3518a93ef2f
RKostochko <kostochko.tech@mail.ru>
2021-08-24 18:29:58 +0300

    fead: task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	// your solution
+	if (str.length % 2 > 0) return false;
+	let iter = str.length / 2;
+
+	let newArr = [];
+	bracketsConfig.forEach((el) => {
+		newArr.push(el.join(''));
+	})
+
+	let strLength = str.length;
+
+	while (iter > 0) {
+		newArr.forEach((el) => {
+			str = str.replace(el, '');
+		});
+
+		if (strLength === str.length) {
+			break;
+		} else {
+			strLength = str.Length;
+			iter--;
+		}
+	}
+	return str.length === 0;
 }
